7f5149a7f488d62fed487bcbb9329e29bde7510c
sakamichi-jewel <taigayyy@gmail.com>
2020-05-12 21:15:54 +0900

    display question, progress bar, and image dynamically in QuestionActivity

--- app/src/main/java/com/narutoshi/quizapp/QuestionActivity.kt
+++ app/src/main/java/com/narutoshi/quizapp/QuestionActivity.kt
@@ -2,6 +2,7 @@ package com.narutoshi.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_question.*
 
 class QuestionActivity : AppCompatActivity() {
 
@@ -10,6 +11,19 @@ class QuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_question)
 
         val questionsList = Questions.getQuestions()
-        println(questionsList[0].id)
+
+        var currentQuestionId = 1
+        var currentQuestion = questionsList[currentQuestionId - 1]
+
+        txtViewQuestionText.text = currentQuestion.question
+        imgViewFlag.setBackgroundResource(currentQuestion.image)
+        progressBar.max = questionsList.size
+        progressBar.progress = currentQuestionId
+        txtViewProgress.text = "$currentQuestionId/${questionsList.size}"
+        txtViewOption1.text = currentQuestion.option1
+        txtViewOption2.text = currentQuestion.option2
+        txtViewOption3.text = currentQuestion.option3
+        txtViewOption4.text = currentQuestion.option4
+
     }
 }
