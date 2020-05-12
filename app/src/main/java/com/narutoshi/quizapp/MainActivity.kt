dc349b0814e33252340813f9e8e98895555715bb
sakamichi-jewel <taigayyy@gmail.com>
2020-05-12 18:49:53 +0900

    add button start click listener functionality

--- app/src/main/java/com/narutoshi/quizapp/MainActivity.kt
+++ app/src/main/java/com/narutoshi/quizapp/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.narutoshi.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart.setOnClickListener {
+            txtInputLayoutName.error = null
+            if(editTextName.text.toString().isEmpty()) {
+                txtInputLayoutName.error = "Please enter your name"
+            } else {
+                val intent = Intent(this, QuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
